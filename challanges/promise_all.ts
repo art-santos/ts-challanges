c697cd8b7e5bb4430734240a19748b235dc45230
Arthur Santos <arthursantosdeveloper@gmail.com>
2023-08-31 04:21:00 -0300

    Add a specific date commit

--- challanges/promise_all.ts
+++ challanges/promise_all.ts
@@ -3,3 +3,8 @@ export default function promiseAll<T extends readonly unknown[] | []>(
   ): Promise<{ -readonly [P in keyof T]: Awaited<T[P]> }> {
     throw 'Not implemented!';
   }
+
+
+  promiseAll([1,2,3,4,5]).then((res) => {
+    console.log(res)
+  })
