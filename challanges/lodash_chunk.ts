9b8f18cb370d9f768d719c3538cf69f2a31b0a43
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-08-13 19:51:15 -0300

    chore: compact

--- challanges/lodash_chunk.ts
+++ challanges/lodash_chunk.ts
@@ -5,12 +5,13 @@ export default function chunk<T extends string>(array: Array<T>, size = 1)
     const result: Array<Array<T>> = [];
 
     for (let i = 0; i < array.length; i += size){
+      console.log(i)
       result.push(array.slice(i, i + size))
     }
 
     return result;
 }
 
-const r = chunk([], 1)
+const r = chunk(["1","2","3", "1"], 3)
 
 console.log(r)
