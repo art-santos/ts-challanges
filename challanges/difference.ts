e01860ce4181c06e4d303215af75666997abe32e
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-08-13 21:00:38 -0300

    chore: difference

---
+++ challanges/difference.ts
@@ -0,0 +1,21 @@
+export default function difference<T>(
+    array: Array<T>,
+    values: Array<T>,
+  ): Array<T> {
+
+    let dif = []
+    for (let i = 0; i <= array.length; i++){
+        values.includes(array[i]) ? null : array[i] !== undefined && dif.push(array[i])
+    }
+
+    return dif
+  }
+
+  const obj = {
+    1: difference([1, 2, 3, null, undefined], [2, 3]), // => [1]
+    2: difference([1, 2, 3, 4], [2, 3, 1]), // => [4]
+    3: difference([1, 2, 3], [2, 3, 1, 4]), // => []
+    4: difference([1, , 3], [1]) // => [3]
+  }
+
+  console.log(obj)
