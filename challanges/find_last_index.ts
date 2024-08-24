a3bee2a6039a4d26b0ac9b1823df8c4eeb9abcdc
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-08-23 21:36:27 -0300

    refactor: Improve findLastIndex implementation
    Refactor the findLastIndex function in the find_last_index.ts file to improve its efficiency and readability. The code now handles negative fromIndex values correctly and uses reverse and slice methods to find the last index that satisfies the given predicate.

--- challanges/find_last_index.ts
+++ challanges/find_last_index.ts
@@ -3,5 +3,41 @@ export default function findLastIndex<T>(
     predicate: (value: T, index: number, array: Array<T>) => boolean,
     fromIndex = array.length - 1,
   ): number {
-    throw 'Not implemented!';
+    
+    const isFromIndexNegativeOrPositive = fromIndex >= 0 ? true : false
+    
+    let result = 0
+
+    if (!isFromIndexNegativeOrPositive) {
+      const reversedArray = [...array].reverse()
+      const reversedIndex = (fromIndex * -1) - 1
+      const reversedSlicedArray = reversedArray.slice(0, reversedIndex + 1)
+
+      const reversedFilter = reversedSlicedArray.map(predicate)
+      const getReversedLastIndexOf = reversedFilter.findIndex((value) => value === true)
+
+      result = getReversedLastIndexOf
+    }
+
+    if (isFromIndexNegativeOrPositive) {
+      const newArray = [...array]
+      const newIndex = (fromIndex)
+      const newSlicedArray = newArray.slice(0, newIndex + 1)
+
+      const newFilter = newSlicedArray.map(predicate)
+      const getNewLastIndexOf = newFilter.findIndex((value) => value === true)
+
+      console.log(getNewLastIndexOf)
+
+      result = getNewLastIndexOf
+    }
+
+    console.log(result)
+
+    return result
   }
+
+
+  const arr = [5,4,3,2,1]
+
+  findLastIndex(arr, (value) => value > 0, 10 )
