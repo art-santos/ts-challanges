73f382b989e79cfda7d8efd65eedf4ac5c1bf4dc
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-09-23 21:33:43 -0300

    test: array prototype at

--- challanges/__tests__/array_prototype_at.test.ts
+++ challanges/__tests__/array_prototype_at.test.ts
@@ -4,8 +4,8 @@ import '../array_prototype_at.ts';
 
 
 
-Deno.test("Should return the array", () => {
+Deno.test("Should return the array in the position 0", () => {
     const test = [1, 2, 3, 4, 5].myAt(0);
     console.log(test);
-    assert(test === test, "Should return the array");
+    assert(test === test, "Should return the array 0th element");
 });
