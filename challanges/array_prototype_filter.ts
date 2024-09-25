14095d33c6cc752531d3791babebdd9fa55a4fd5
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-09-24 22:02:10 -0300

    chore: array prototype filter

---
+++ challanges/array_prototype_filter.ts
@@ -0,0 +1,16 @@
+declare global {
+
+interface Array<T> {
+    myFilter(
+      callbackFn: (value: T, index: number, array: Array<T>) => boolean,
+      thisArg?: any,
+    ): Array<T>;
+  }
+}
+ 
+
+Array.prototype.myFilter = function (callbackFn, thisArg) {
+    throw 'Not implemented!';
+};
+
+export {};
