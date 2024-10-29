17f5d22dc50ce062da7fb453c9665d65171ec7dd
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-10-28 21:39:49 -0300

    refactor: event emmiter comments

--- challanges/event_emitter_II.ts
+++ challanges/event_emitter_II.ts
@@ -19,3 +19,26 @@ interface IEventEmitter {
       throw 'Not implemented!';
     }
   }
+
+//   const emitter = new EventEmitter();
+
+// function addTwoNumbers(a, b) {
+//   console.log(`The sum is ${a + b}`);
+// }
+
+// // Returns a subscription object that has an .off() method.
+// const sub = emitter.on('foo', addTwoNumbers);
+// emitter.emit('foo', 2, 5);
+// // > "The sum is 7"
+
+// emitter.on('foo', (a, b) => {
+//   console.log(`The product is ${a * b}`);
+// });
+// emitter.emit('foo', 4, 5);
+// // > "The sum is 9"
+// // > "The product is 20"
+
+// sub.off(); // This unsubscribes the callback that logs the sum of the numbers.
+// emitter.emit('foo', -3, 9);
+// // > "The product is -27"
+// // (Only the multiply callback is triggered, the first one was unsubscribed.)
