f69470fd1d6f26e502662e6ebfd3a0628dff57de
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-10-31 22:45:46 -0300

    refactor: getElementsByTagName comments

--- challanges/getElementsByTagName.ts
+++ challanges/getElementsByTagName.ts
@@ -14,3 +14,16 @@ export default function getElementsByTagName(
     throw 'Not implemented!';
   }
 
+//   Examples
+
+//   const doc = new DOMParser().parseFromString(
+//     `<div id="foo">
+//       <span>Span</span>
+//       <p>Paragraph</p>
+//       <div id="bar">Div</div>
+//     </div>`,
+//     'text/html',
+//   );
+  
+//   getElementsByTagName(doc.body, 'div');
+//   [div#foo, div#bar] <-- This is an array of elements.
