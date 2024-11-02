2e468d974605ab10af664b8fb7d760d3b25e5b08
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-01 21:04:54 -0300

    refactor: HTML Serializer comments

--- challanges/HTMLSerializer.ts
+++ challanges/HTMLSerializer.ts
@@ -3,3 +3,29 @@ type Element = { tag: string; children: Array<string | Element> };
 export default function serializeHTML(element: Element): string {
   throw 'Not implemented!';
 }
+
+const tree = {
+    tag: 'body',
+    children: [
+      { tag: 'div', children: [{ tag: 'span', children: ['foo', 'bar'] }] },
+      { tag: 'div', children: ['baz'] },
+    ],
+  };
+  
+  serializeHTML(tree);
+  // Output:
+  `<body>
+    <div>
+      <span>
+        foo
+        bar
+      </span>
+    </div>
+    <div>
+      baz
+    </div>
+  </body>`;
+
+  
+
+  
