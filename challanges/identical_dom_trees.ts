b84ba227ff0662d2564b03708f7795cf8b17fd66
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-02 23:26:48 -0300

    docs: identical DOM Trees comments

--- challanges/identical_dom_trees.ts
+++ challanges/identical_dom_trees.ts
@@ -8,3 +8,22 @@ declare global {
 export default function identicalDOMTrees(nodeA: Node, nodeB: Node): boolean {
     throw 'Not implemented!';
 }
+
+
+
+// Examples
+// Tree A and Tree B are considered the same.
+
+// <!-- Tree A -->
+// <div>Hello World</div>
+
+// <!-- Tree B -->
+// <div>Hello World</div>
+// Tree C and Tree D are considered the different.
+
+
+// <!-- Tree C -->
+// <div class="header">Hello World</div>
+
+// <!-- Tree D -->
+// <div id="foo">Hello World</div>
