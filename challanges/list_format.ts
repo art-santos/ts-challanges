a10a0482df1b139daf797b1777d8d949c003c48a
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-04 21:20:59 -0300

    docs: list format comments

--- challanges/list_format.ts
+++ challanges/list_format.ts
@@ -12,69 +12,7 @@ export default function listFormat(
 // listFormat(['Bob', 'Alice']); // 'Bob and Alice'
 
 // listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John']);
-// // 'Bob, Ben, Tim, Jane and John'
-
-// listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John'], {
-//   length: 3,
-// }); // 'Bob, Ben, Tim and 2 others'
-
-// listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John'], {
-//   length: 4,
-// }); // 'Bob, Ben, Tim, Jane and 1 other'
-
-// listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John'], {
-//   length: 3,
-//   sorted: true,
-// }); // 'Ben, Bob, Jane and 2 others'
-
-// listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John', 'Bob'], {
-//   length: 3,
-//   unique: true,
-// }); // 'Bob, Ben, Tim and 2 others'
-
-// listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John'], {
-//   length: 3,
-//   unique: true,
-// }); // 'Bob, Ben, Tim and 2 others'
-
-// listFormat(['Bob', 'Ben', '', '', 'John']); // 'Bob, Ben and John'listFormat([]); // ''
-
-// listFormat(['Bob']); // 'Bob'
-// listFormat(['Bob', 'Alice']); // 'Bob and Alice'
-
-// listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John']);
-// // 'Bob, Ben, Tim, Jane and John'
-
-// listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John'], {
-//   length: 3,
-// }); // 'Bob, Ben, Tim and 2 others'
-
-// listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John'], {
-//   length: 4,
-// }); // 'Bob, Ben, Tim, Jane and 1 other'
-
-// listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John'], {
-//   length: 3,
-//   sorted: true,
-// }); // 'Ben, Bob, Jane and 2 others'
-
-// listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John', 'Bob'], {
-//   length: 3,
-//   unique: true,
-// }); // 'Bob, Ben, Tim and 2 others'
-
-// listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John'], {
-//   length: 3,
-//   unique: true,
-// }); // 'Bob, Ben, Tim and 2 others'
-
-// listFormat(['Bob', 'Ben', '', '', 'John']); // 'Bob, Ben and John'listFormat([]); // ''
-
-// listFormat(['Bob']); // 'Bob'
-// listFormat(['Bob', 'Alice']); // 'Bob and Alice'
-
-// listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John']);
-// // 'Bob, Ben, Tim, Jane and John'
+// 'Bob, Ben, Tim, Jane and John'
 
 // listFormat(['Bob', 'Ben', 'Tim', 'Jane', 'John'], {
 //   length: 3,
