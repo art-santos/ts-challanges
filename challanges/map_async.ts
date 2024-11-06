dc629915ec6e3d67f304e3e7f8b4fe50dd02c275
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-05 21:42:09 -0300

    docs: map async comments

--- challanges/map_async.ts
+++ challanges/map_async.ts
@@ -4,3 +4,20 @@ export default function mapAsync<T, U>(
   ): Promise<Array<U>> {
     throw 'Not implemented';
 }
+
+//RELEVANT LINKS:
+
+//https://www.greatfrontend.com/questions/javascript/array-map
+
+
+//HOW THE CODE SHOULD LOOK LIKE:
+
+// const asyncDouble = (x: number) =>
+//     new Promise((resolve) => {
+//       setTimeout(() => {
+//         resolve(x * 2);
+//       }, 10);
+//     });
+  
+//   const doubled = await mapAsync([1, 2], asyncDouble);
+//   console.log(doubled); // [2, 4]
