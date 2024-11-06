ec84bcfd67cd5310afd0d0e856733858ce918212
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-06 20:13:16 -0300

    DOCS: promisse all settled comments

--- challanges/promise_all_settled.ts
+++ challanges/promise_all_settled.ts
@@ -13,3 +13,20 @@ interface PromiseFulfilledResult<T> {
   ): Promise<Array<PromiseFulfilledResult<T> | PromiseRejectedResult>> {
     throw 'Not implemented!';
 }
+
+//HOW THE CODE SHOULD LOOK LIKE
+// const p0 = Promise.resolve(3);
+// const p1 = 42;
+// const p2 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     reject('foo');
+//   }, 100);
+// });
+//
+// await promiseAllSettled([p0, p1, p2]);
+//WHAT IT MUST RETURN
+// [
+//   { status: 'fulfilled', value: 3 },
+//   { status: 'fulfilled', value: 42 },
+//   { status: 'rejected', reason: 'foo' },
+// ];
