8ae78ca3ff1034cf9e61d0bac155d5df8b7c58b1
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-07 23:44:19 -0300

    chore: resumable interval

---
+++ challanges/resumable_interval.ts
@@ -0,0 +1,15 @@
+interface Resumable {
+    start: () => void;
+    pause: () => void;
+    stop: () => void;
+  }
+  
+  export default function createResumableInterval(
+    callback: Function,
+    delay?: number,
+    ...args: Array<any>
+  ): Resumable {
+    throw 'Not implemented';
+  }
+
+  
