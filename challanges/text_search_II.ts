c39ae3da959b42549b600d46fdba2c717370a7a4
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-12 20:24:26 -0300

    chore: text_search_II

---
+++ challanges/text_search_II.ts
@@ -0,0 +1,8 @@
+export default function textSearch(
+    text: string,
+    queries: Array<string>,
+  ): string {
+    throw 'Not implemented!';
+}
+
+textSearch('The Quick Brown Fox Jumps Over The Lazy Dog', ['fox']);
