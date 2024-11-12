e10c6c4b015bbcf394a22892e52e9082498d9048
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-11 22:33:45 -0300

    chore: map async limit

---
+++ challanges/map_async_limit.ts
@@ -0,0 +1,9 @@
+export default function mapAsyncLimit<T, U>(
+    iterable: Array<T>,
+    callbackFn: (value: T) => Promise<U>,
+    size?: number,
+  ): Promise<Array<U>> {
+    throw 'Not implemented';
+}
+
+
