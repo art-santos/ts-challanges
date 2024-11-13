f703a434ec0b1da36a346194a5960574d0f4c342
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-12 22:18:42 -0300

    docs: deep merge comments

--- challanges/deep_merge.ts
+++ challanges/deep_merge.ts
@@ -1,3 +1,9 @@
 export default function deepMerge(valA: unknown, valB: unknown): unknown {
     throw 'Not implemented';
 }
+
+
+//HOW THE CODE MUST LOOK LIKE:
+deepMerge({ a: 1 }, { b: 2 }); // { a: 1, b: 2 }
+deepMerge({ a: 1 }, { a: 2 }); // { a: 2 }
+deepMerge({ a: 1, b: [2] }, { b: [3, 4] }); // { a: 1, b: [2, 3, 4] }
