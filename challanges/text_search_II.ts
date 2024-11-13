3b3be3fd5b8bc303ceaf5ccc87c2ffa649ba199a
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-12 22:17:54 -0300

    chore: deep merge

--- challanges/text_search_II.ts
+++ challanges/text_search_II.ts
@@ -6,3 +6,17 @@ export default function textSearch(
 }
 
 textSearch('The Quick Brown Fox Jumps Over The Lazy Dog', ['fox']);
+
+//CODE EXAMPLES:
+
+// textSearch('This is Uncopyrightable!', ['copy', 'right']);
+// 'This is Un<b>copyright</b>able!'
+// textSearch('This is Uncopyrightable!', ['copy', 'right', 'table']);
+// 'This is Un<b>copyrightable</b>!'
+
+
+// textSearch('aaa', ['aa']);
+// '<b>aa</b>a'
+// This is because the second character cannot be used as a match again.
+// textSearch('aaaa', ['aa']);
+// '<b>aaaa</b>'
