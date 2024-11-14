0e81736ca9f56bbae54bdfdd418674f0f075aaa8
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-13 23:41:58 -0300

    docs: deep equal comments

--- challanges/data_equal.ts
+++ challanges/data_equal.ts
@@ -1,3 +1,9 @@
 export default function deepEqual(valueA: unknown, valueB: unknown): boolean {
     throw 'Not implemented!';
 }
+
+
+// deepEqual('foo', 'foo'); // true
+// deepEqual({ id: 1 }, { id: 1 }); // true
+// deepEqual([1, 2, 3], [1, 2, 3]); // true
+// deepEqual([{ id: '1' }], [{ id: '2' }]); // false
