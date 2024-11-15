12b738a782a0ca97d23269471e1b011140e31189
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-14 22:21:29 -0300

    docs: get elements by class name comments

--- challanges/get_elements_by_class_name.ts
+++ challanges/get_elements_by_class_name.ts
@@ -4,3 +4,20 @@ export default function getElementsByClassName(
   ): Array<Element> {
     throw 'Not implemented!';
 }
+
+
+//HOW THE CODE SHOULD LOOK LIKE
+
+
+// const doc = new DOMParser().parseFromString(
+//     `<div class="foo bar baz">
+//       <span class="bar baz">Span</span>
+//       <p class="foo baz">Paragraph</p>
+//       <div class="foo bar"></div>
+//     </div>`,
+//     'text/html',
+//   );
+  
+//   getElementsByClassName(doc.body, 'foo bar');
+//   // [div.foo.bar.baz, div.foo.bar] <-- This is an array of elements.
+  
