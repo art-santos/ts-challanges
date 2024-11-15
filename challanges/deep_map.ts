bd0f0f6fff585f3e75ee3cb983170292c4653928
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-14 22:18:28 -0300

    docs: deep map comments

--- challanges/deep_map.ts
+++ challanges/deep_map.ts
@@ -3,3 +3,19 @@ export default function deepMap(value: unknown, fn: Function): unknown {
 }
 
 
+//HOW THE CODE SHOULD LOOK LIKE:
+
+
+// const double = (x) => x * 2;
+
+// deepMap(2, double); // 4
+// deepMap([1, 2, 3], double); // [4, 5, 6]
+// deepMap({ a: 1, b: 2, c: 3 }, double); // { a: 2, b: 4, c: 6 }
+// deepMap(
+//   {
+//     foo: 1,
+//     bar: [2, 3, 4],
+//     qux: { a: 5, b: 6 },
+//   },
+//   double,
+// ); // => { foo: 2, bar: [4, 6, 8], qux: { a: 10, b: 12 } }
