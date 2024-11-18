dd87397719e15aabb05a142d00bb85d7a46870ac
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-17 22:51:21 -0300

    docs: class names II comments

--- challanges/class_names_II.ts
+++ challanges/class_names_II.ts
@@ -13,3 +13,11 @@ export type ClassArray = Array<ClassValue>;
 export default function classNames(...args: Array<ClassValue>): string {
   throw 'Not implemented!';
 }
+
+//HOW THE CODE SHOULD LOOK LIKE:
+
+// classNames('foo', 'foo'); // 'foo'
+// classNames({ foo: true }, { foo: true }); // 'foo'
+// classNames({ foo: true, bar: true }, { foo: false }); // 'bar'
+// classNames('foo', () => 'bar'); // 'foo bar'
+// classNames('foo', () => 'foo'); // 'foo'
