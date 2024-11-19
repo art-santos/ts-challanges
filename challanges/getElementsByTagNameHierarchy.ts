bafdd38f2ef997d2da1c328665e7b6c2c172f4e4
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-18 21:23:02 -0300

    chore: getDocumentsByTagHierarchy

--- challanges/getElementsByTagNameHierarchy.ts
+++ challanges/getElementsByTagNameHierarchy.ts
@@ -1,3 +1,5 @@
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+
 type Document = any
 
 export default function getElementsByTagNameHierarchy(
@@ -6,3 +8,18 @@ export default function getElementsByTagNameHierarchy(
   ): Array<Element> {
     throw 'Not implemented!';
 }
+
+
+const doc = new DOMParser().parseFromString(
+    `<div>
+      <span id="foo">
+        <span id="bar">Bar</span>
+        Foo
+      </span>
+      <p>Paragraph</p>
+      <span id="baz">Baz</span>
+    </div>`,
+    'text/html',
+  );
+  
+  getElementsByTagNameHierarchy(doc, 'div span');
