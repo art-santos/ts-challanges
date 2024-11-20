e85051f20cd39c3df94637ae64c3b11a905f3cda
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-20 18:43:49 -0300

    chore: deep clone II

--- challanges/deep_clone_II.ts
+++ challanges/deep_clone_II.ts
@@ -4,3 +4,28 @@ export default function deepClone<T>(value: T): T {
 }
 
 
+const obj1 = {
+    num: 0,
+    str: '',
+    boolean: true,
+    unf: undefined,
+    nul: null,
+    obj: { name: 'foo', id: 1 },
+    arr: [0, 1, 2],
+    date: new Date(),
+    reg: new RegExp('/bar/ig'),
+    [Symbol('s')]: 'baz',
+  };
+  
+  const clonedObj1 = deepClone(obj1);
+  clonedObj1.arr.push(3);
+  obj1.arr; // Should still be [0, 1, 2]
+  const obj2: any = { a: {} };
+  obj2.a.b = obj2; // Circular reference
+  
+  const clonedObj2 = deepClone(obj2); // Should not cause a stack overflow by recursing into an infinite loop.
+  
+  (clonedObj2.a as any).b = 'something new';
+  
+  obj2.a.b === obj2; // This should still be true
+  
