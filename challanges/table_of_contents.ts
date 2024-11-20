68da74bc15b516379168a416ba8025dfe11aabae
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-19 22:40:57 -0300

    chore: table of contents

---
+++ challanges/table_of_contents.ts
@@ -0,0 +1,7 @@
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+
+type Document = any
+
+export default function tableOfContents(doc: Document): string {
+    throw 'Not implemented!';
+}
