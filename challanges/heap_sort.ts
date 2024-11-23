faec1a58820b09515cf39625e15106fed7e48f04
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-23 20:48:13 -0300

    chore: heap sort

--- challanges/heap_sort.ts
+++ challanges/heap_sort.ts
@@ -3,3 +3,5 @@ export default function heapSort(arr: Array<number>): Array<number> {
 }
 
 
+heapSort([9, 3, 6, 2, 1, 11]); // [1, 2, 3, 6, 9, 11]
+heapSort([12, 16, 14, 1, 2, 3]); // [1, 2, 3, 12, 14, 16]
