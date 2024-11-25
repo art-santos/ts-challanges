0ccc264447660781e43b7d819f065428f184b2db
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-24 22:51:37 -0300

    chore: queue

--- challanges/queue.ts
+++ challanges/queue.ts
@@ -53,3 +53,14 @@ export default class Queue<T> {
     }
   }
   
+
+    const queue = new Queue();
+    queue.isEmpty(); // true
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.length(); // 2
+    queue.enqueue(3);
+    queue.front(); // 1
+    queue.back(); // 3
+    queue.dequeue(); // 1
+    queue.isEmpty(); // false
