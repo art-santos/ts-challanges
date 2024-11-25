0b3fc502873d5d31fd11d8d986b4a97bff01bd59
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-11-25 20:33:45 -0300

    chore: merge sort

--- challanges/merge_sort.ts
+++ challanges/merge_sort.ts
@@ -1,3 +1,10 @@
 export default function recursiveMergeSort(arr: Array<number>): Array<number> {
     throw 'Not implemented!';
 }
+
+const mergeSort = (arr: Array<number>) => {
+    return recursiveMergeSort(arr)
+}
+
+mergeSort([9, 3, 6, 2, 1, 11]); // [1, 2, 3, 6, 9, 11]
+mergeSort([12, 16, 14, 1, 2, 3]); // [1, 2, 3, 12, 14, 16]
