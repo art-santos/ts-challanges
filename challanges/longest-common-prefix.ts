ff440c7aee43d5b5900b7b7efabc55ab8b93dd00
art-santos <arthursantosdeveloper@gmail.com>
2025-01-13 22:51:43 -0300

    chore: longest common prefix

---
+++ challanges/longest-common-prefix.ts
@@ -0,0 +1,28 @@
+function longestCommonPrefix(strs: string[]) {
+
+    if(strs.length === 1){
+    return strs[0]
+    }
+
+    const allEqual = arr => arr.every(val => val === arr[0]);
+
+    if(allEqual(strs)){
+        return strs[0]
+    }
+
+    const byteSize = str => new Blob([str]).size;
+
+   for (let i = 0; i < strs.length; i++){
+    let j = 0
+       do {
+        console.log(strs[i][j])
+        console.log(j)
+        console.log("here", byteSize(strs[i]))
+        j++
+       }while(j <= byteSize[strs[i]])
+   }
+};
+
+console.log(longestCommonPrefix(["ab", "abc", "abcd"])); // Output: "fl"
+
+
